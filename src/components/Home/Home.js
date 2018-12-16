e5733db5198a03a9fef19ce8829aee0a6efb1ce5
Avinash Dhillor <avinashdhillor24@gmail.com>
2018-12-16 19:23:23 +0530

    made search and fetching data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MoviesCard from './MoviesCard';
 
-const Home = () => {
-  return ( 
-    <h1>Welcome to Movie Land!</h1>
-   );
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      searchText: '',
+      movies: []
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    let value = e.target.value;
+    this.setState({
+      input: value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState(prevState => ({
+      searchText: prevState.input
+    }));
+    e.target.elements.search.value = '';
+
+    fetch('http://www.omdbapi.com/?s=superman%20vs%20batman&apikey=318c98f3')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.Search);
+        this.setState({
+          movies: data
+        });
+      });
+  }
+
+  render() {
+    let renderContent;
+    if (this.state.movies.length === 0) {
+      renderContent = <h1>Show top 10 movies</h1>;
+    } else {
+      renderContent = <MoviesCard />;
+    }
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input type='text' name='search' onChange={this.onChange} />
+          <button type='submit'>Search</button>
+        </form>
+        {renderContent}
+      </div>
+    );
+  }
 }
- 
+
 export default Home;
---
+++ src/components/Home/MoviesCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MoviesCard() {
+  return (
+    <div>
+      <h1>This will show movies card on page</h1>
+    </div>
+  );
+}
